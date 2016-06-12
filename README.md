fefe0dc4378397694de63b2995c5cd6a60a85d14
Brandon Surmanski <b.surmanski@gmail.com>
2016-06-12 14:15:10 -0700

    Add README, add figure

---
+++ README.md
@@ -0,0 +1,3 @@
+# Comparison Between Income Tax in Toronto and SF
+
+[[https://github.com/bsurmanski/TorontoSFTax/blob/master/img/figure_1.png]]
--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 import tax
 
+fig, ax = plt.subplots()
+ax.grid(True)
 t = range(0, 300000, 100)
+plt.xticks(range(0, 300000, 20000))
+plt.yticks(range(0, 300000, 20000))
 plt.title('Tax Toronto vs SF')
 plt.xlabel('$Earned')
 plt.ylabel('$Tax')
 to_tax = plt.plot(t, [tax.TorontoTax(v) for v in t], 'r', label='Toronto')
 sf_tax = plt.plot(t, [tax.SFTax(v) for v in t], 'b', label='SF')
-plt.legend()
+plt.legend(loc=4)
 plt.show()
